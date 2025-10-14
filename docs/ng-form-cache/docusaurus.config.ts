c1ca6a8890857a5dad07710a5b3964922c542166
Aaron Czichon <aaron.czichon@plan-b-gmbh.com>
2025-10-14 17:45:44 +0200

    chore: updated docusaurus version

--- docs/ng-form-cache/docusaurus.config.ts
+++ docs/ng-form-cache/docusaurus.config.ts
@@ -30,7 +30,6 @@ const config: Config = {
 	projectName: 'pb-dpf-ng-form-cache-library', // Usually your repo name.
 
 	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
 
 	// Even if you don't use internationalization, you can use this field to set
 	// useful metadata like html lang. For example, if your site is Chinese, you
